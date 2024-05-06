53131d7d66aabae5d383001e824953182ad9b33d
4ahmed <axmedarnab3@gmail.com>
2024-05-06 04:58:12 -0700

    handling unhandled errors, and shutting down the server

--- server.js
+++ server.js
@@ -2,4 +2,24 @@ const app = require('./app');
 
 // STARTING APPLICATION
 const PORT = 3000;
-app.listen(PORT, () => console.log(`SERVER IS RUNNING ON PORT ${PORT}`));
+const server = app.listen(PORT, () =>
+  console.log(`SERVER IS RUNNING ON PORT ${PORT}`)
+);
+
+// Handling unhandled REJECTION
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION, SHUTTING DOWN...');
+
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+// Handling Uncaught exceptions
+process.on('uncaughtExceptionMonitor', (err) => {
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
